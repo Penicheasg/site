54b5e76ddf8dc77fa15f667740b9286eed0b3da0
Penicheasg <92694808+Penicheasg@users.noreply.github.com>
2025-03-07 19:14:54 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,18 +3,22 @@
     sudo tcpdump -nn -i "$IFACE" -c 20 vlan 2>/dev/null | awk -F'vlan ' '/vlan/ {print $2}' | awk '{print $1}' | sort -u |
     { while read VLAN; do
         echo "[+] VLAN detectada: $VLAN - Criando interface..."
-        sudo ip link add link "$IFACE" name "$IFACE.$VLAN" type vlan id "$VLAN" && sudo ip link set "$IFACE.$VLAN" up
+        sudo ip link add link "$IFACE" name "$IFACE.$VLAN" type vlan id "$VLAN"
+        sudo ip link set "$IFACE.$VLAN" up
         sudo dhclient "$IFACE.$VLAN" -v 2>/dev/null || echo "[!] DHCP falhou, tentando IP manual..."
 
         for NET in "192.168.$VLAN.0/24" "10.$VLAN.0.0/16" "172.16.$VLAN.0/24"; do
             echo "[*] Escaneando hosts ativos na rede $NET..."
+            sleep 1  # Adiciona um tempo de espera
             { sudo nmap -sn -e "$IFACE.$VLAN" "$NET" 2>/dev/null || sudo nmap -sn -Pn -e "$IFACE.$VLAN" "$NET" 2>/dev/null; } |
-            awk '/Nmap scan report/{print $5}'
+            tee scan_hosts.txt | awk '/Nmap scan report/{print $5}'
         done |
         while read IP; do
             [ -z "$IP" ] && continue
             echo "[+] Host encontrado: $IP - Verificando portas abertas..."
-            sudo nmap -sS --top-ports 50 -T2 -e "$IFACE.$VLAN" "$IP" 2>/dev/null || sudo nmap -sS -Pn --top-ports 50 -T2 -e "$IFACE.$VLAN" "$IP" 2>/dev/null
+            sleep 1  # Pausa antes de cada scan de portas
+            { sudo nmap -sS --top-ports 50 -T2 -e "$IFACE.$VLAN" "$IP" 2>/dev/null || sudo nmap -sS -Pn --top-ports 50 -T2 -e "$IFACE.$VLAN" "$IP" 2>/dev/null; } |
+            tee scan_ports.txt
         done
 
         echo "[!] Removendo VLAN $VLAN"
